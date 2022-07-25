6653bddfec0c4c2bf3c74c304d9a5d06d1cf8655
David Gaither <dgaither@instructors.2u.com>
2022-07-25 15:46:02 -0400

    Nav Bar

---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+export default function Navbar() {
+    return (
+        <header className="">
+            <div className="">
+
+                <h1>David Gaither</h1>
+
+                <nav className="">
+                    <a href="#about" className="">
+                        About Me
+                    </a>
+                    <a href="#projects" className="">
+                        Projects
+                    </a>
+                    <a href="#skills" className="">
+                        Technical Skills
+                    </a>
+                    <a
+                        href="#contact"
+                        className="">
+                        Contact Me
+                    </a>
+
+                    <a href="">Resume</a>
+                </nav>
+            </div>
+        </header>
+    );
+}
